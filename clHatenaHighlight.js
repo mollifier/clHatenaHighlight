27963bc7c11b4d0b21d02bc7aea330c234cc7d22
mollifier <mollifier@gmail.com>
2009-09-05 12:31:28 +0900

    Remove (function() { ... })();

--- clHatenaHighlight.js
+++ clHatenaHighlight.js
@@ -1,14 +1,12 @@
-(function() {
-  jetpack.statusBar.append({
-    html: "clearH",
-    width: 60,
-    onReady: function(widget) {
-      $(widget).click(function() {
-        $(jetpack.tabs.focused.contentDocument).
-          find("span.highlight").removeClass("highlight");
-      });
-    }
-  });
-})();
+jetpack.statusBar.append({
+  html: "clearH",
+  width: 60,
+  onReady: function(widget) {
+    $(widget).click(function() {
+      $(jetpack.tabs.focused.contentDocument).
+      find("span.highlight").removeClass("highlight");
+    });
+  }
+});
 
 // vim:set ts=2 sw=2:
